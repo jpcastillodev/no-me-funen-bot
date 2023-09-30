e009376b0fd8289ba8adde4514a656cea42d46ca
Juan Castillo <Juanpablodlcc@gmail.com>
2023-09-29 23:11:32 -0600

    []

--- commands/play.js
+++ commands/play.js
@@ -3,15 +3,8 @@ module.exports.run = async (bot, message, args) => {
         if (!message.member.voice.channel) return message.channel.send('No estas en un canal imbecil');
 
         const music = args.join(" ");
-
-        let queue = await bot.distube.getQueue(message);
-        console.log(queue)
-        if (queue) {
-
-        } else {
-
-            bot.distube.play(message.member.voice.channel, music, { message })
-        }
+        console.log("music", music)
+        bot.distube.play(message.member.voice.channel, music, { message })
     } catch (err) {
         console.log(err)
     }
